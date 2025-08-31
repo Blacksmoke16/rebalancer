c1b919f1f7006b95099afcd5af1018d63492e024
George Dietrich <dev@dietrich.pub>
2025-08-31 11:10:31 -0400

    Try with cloudflare vite plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,14 @@
 /// <reference types="vitest" />
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { cloudflare } from "@cloudflare/vite-plugin";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), cloudflare()],
   test: {
     globals: true,
-    environment: 'jsdom',
-    setupFiles: ['./src/test/setup.ts'],
+    environment: "jsdom",
+    setupFiles: ["./src/test/setup.ts"],
   },
-})
+});
