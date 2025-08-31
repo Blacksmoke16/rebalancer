4db29d281620ae34a5a194f27d5fa20c69160563
George Dietrich <dev@dietrich.pub>
2025-08-31 11:17:48 -0400

    Switch to CF Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,10 @@
 /// <reference types="vitest" />
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { cloudflare } from "@cloudflare/vite-plugin";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), cloudflare()],
+  plugins: [react()],
   test: {
     globals: true,
     environment: "jsdom",
